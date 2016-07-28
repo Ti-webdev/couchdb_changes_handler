66b4a09a290fd8daae2d806cd9a54e65b9ae10b7
Vasily Mikhaylovsky <vasiliy.mikhaylovsky@gmail.com>
2016-07-28 11:41:35 +0500

    added seqKey

---
+++ CouchDbChangeHundler.js
@@ -0,0 +1,71 @@
+const follow = require('follow')
+const debug = require('debug')
+
+const PouchDB = require('pouchdb-core')
+  .plugin(require('pouchdb-adapter-http'))
+  .plugin(require('pouchdb-upsert'))
+
+class CouchDbChangeHundler {
+  constructor() {
+    this.logTag = 'cch'
+    this.seqKey = 'seq'
+  }
+
+  start() {
+    this._logInfo = debug(`${this.logTag}:info`)
+    this._logError = debug(`${this.logTag}:error`)
+    this._db = new PouchDB(this.options.db)
+
+    this._logInfo('get last seq')
+    return this._db.get(this.seqId)
+      .then(seqDoc => {
+        this._logInfo('start since', seqDoc[this.seqKey])
+        this.options.since = seqDoc[this.seqKey]
+      }, error => {
+        if ('not_found' === error.name) {
+          this._logInfo('starting with first seq')
+        } else {
+          this._logError('Cannot get seq', error)
+          throw error
+        }
+      })
+      .then(() => this._startFollow())
+      .catch(error => this._logError('error get seq', error))
+  }
+
+  _startFollow() {
+    this._logInfo('starting follow', this.options)
+
+    let cch = this
+    this._feed = follow(this.options, (error, change) => {
+      this._feed.pause()
+      cch._onChange(error, change)
+        .then(() => {
+          this._logInfo('feed resume')
+          this._feed.resume()
+        })
+    })
+  }
+
+  _onChange(error, change) {
+    return Promise.resolve()
+      .then(() => this._handler(error, change))
+      .then(() => {
+        if (error) {
+          return
+        }
+        return this._db.upsert(this.seqId, function (docSeq) {
+          if (!docSeq[this.seqKey] || docSeq[this.seqKey] < change.seq) {
+            docSeq[this.seqKey] = change.seq
+            this._logInfo('save seq', docSeq[this.seqKey])
+            return docSeq
+          }
+          else {
+            this._logError('seq is wrong', docSeq[this.seqKey])
+          }
+        })
+      })
+  }
+}
+
+module.exports = CouchDbChangeHundler
--- index.js
+++ index.js
@@ -1,68 +1,16 @@
 'use strict'
 
-const follow = require('follow')
-const debug = require('debug')
-
-const PouchDB = require('pouchdb-core')
-  .plugin(require('pouchdb-adapter-http'))
-  .plugin(require('pouchdb-upsert'))
-
-const console = {
-  info: debug('cch:info'),
-  error: debug('cch:error')
-}
+const CouchDbChangeHundler = require('./CouchDbChangeHundler')
 
 module.exports = function (seqId, followOptions, handler) {
-  let db = new PouchDB(followOptions.db)
-
-  let startFollow = function () {
-    console.info('starting follow', followOptions)
-    follow(followOptions, function(error, change) {
-      let feed = this
-      feed.pause()
-
-      let result = handler(error, change, feed)
-
-      Promise.resolve(result)
-      .then(function () {
-        if (error) {
-          return
-        }
-        return db.upsert(seqId, function (docSeq) {
-          if (!docSeq.seq || docSeq.seq < change.seq) {
-            docSeq.seq = change.seq
-            console.info('save seq', docSeq.seq)
-            return docSeq
-          }
-          else {
-            console.error('seq is wrong', docSeq.seq)
-          }
-        })
-      })
-      .then(function () {
-        console.info('feed resume')
-        feed.resume()
-      })
-    })
+  let cch = new CouchDbChangeHundler()
+  if (arguments.length) {
+    cch.seqId = seqId
+    cch.followOptions = followOptions
+    cch.handler = handler
+    return cch.start()
   }
-
-  console.info('get last seq')
-  return db.get(seqId)
-    .then(function (seqDoc) {
-      console.info('start since', seqDoc.seq)
-      followOptions.since = seqDoc.seq
-    }, function (error) {
-      if ('not_found' === error.name) {
-        console.info('starting with first seq')
-      } else {
-        console.error('Cannot get seq', error)
-        throw error
-      }
-    })
-    .then(function () {
-      return startFollow()
-    })
-    .catch(function (error) {
-      console.error('error get seq', error)
-    })
+  else {
+    return cch
+  }
 }
